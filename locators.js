a6ed2ed50415e6508fcf9b31daa29a58b573d99c
Leonard Mustafa <leoonardmustafa@gmail.com>
2025-11-10 02:27:16 +0100

    add to cart added

--- locators.js
+++ locators.js
@@ -5,6 +5,14 @@ export default {
     loginbtn: "#login-button",
     inventorylist: "div[data-test='inventory-list']",
     errorlogin: "div[class='error-message-container error']",
+    slbackpack: "button[data-test='add-to-cart-sauce-labs-backpack']",
+    inventorylist: "div[data-test='inventory-item-name']",
+    inventoryprice: "div[data-test='inventory-item-price']",
+    cartItem: "div[data-test='inventory-item']",
+    continue: "input[data-test='continue']",
+    finish: "button[data-test='finish']",
+    
+
 
 
 
--- zaratest.spec.js
+++ zaratest.spec.js
@@ -24,5 +24,38 @@ test.describe("Zara test", () =>   {
     await expect(page.locator(locators.errorlogin)).toBeVisible();
     })
 
+    // Login si standard_user.
+    // Në faqen e produkteve, klik Add to cart për një produkt (p.sh. Sauce Labs Backpack).
+    // Kliko ikonën e shportës (Cart) dhe hap Your Cart. Verifiko që produkti ekziston dhe 
+    // çmimi është i saktë.
+    // Kliko Checkout → plotëso First Name, Last Name, Postal Code → Continue → Finish.
+    // Verifiko që shfaqet konfirmimi THANK YOU FOR YOUR ORDER.
+    test("Add to cart, checkout", async ({ page }) => {
+        await page.fill("#user-name", "standard_user");
+        await page.fill("#password", "secret_sauce");
+        await page.locator(locators.loginbtn).click();
+        await expect(page).toHaveURL(/inventory.html/);
+        await page.locator(locators.slbackpack).click();
+        await expect(page.locator(".shopping_cart_badge")).toHaveText("1");
+        await page.locator(".shopping_cart_link").click();
+        await expect(page.locator(locators.inventorylist)).toBeVisible();
+        const item = await page.locator(locators.cartItem);
+        await expect(item).toContainText("Sauce Labs Backpack");
+        await expect(item).toContainText("29.99");
+        await page.locator("#checkout").click();
+        await page.fill("#first-name", "Leonard");
+        await page.fill("#last-name", "Mustafa");
+        await page.fill("#postal-code", "12050");
+        await page.locator(locators.continue).click();
+        await expect(page).toHaveURL(/checkout-step-two.html/);
+        await page.locator(locators.finish).click();
+        await expect(page).toHaveURL(/checkout-complete.html/);
+        
+
+
+
+
+
+    })
 
 })
